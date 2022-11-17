5d7c9f9dee7662122d2fef386504b58ed38da6f7
Emilia Rojo <emiliamrojo@gmail.com>
2022-11-17 21:49:23 +0100

    Created skeleton of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: [:show, :edit, :update, :destroy]
+  get 'movies/index', to: 'movies#index'
 end
